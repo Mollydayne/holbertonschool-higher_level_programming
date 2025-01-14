bd5d04688ef5eee3d3b4cc32bace159e50441037
Mollydayne <cl.w.perez@gmail.com>
2025-01-14 21:21:49 +0100

    Redoing task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    return ord('A') <= ord(c) <= ord('Z')
+def uppercase(str):
+    for char in str:
+        asciiVal = ord(char)
+        upperChar = chr(asciiVal - 32) if 97 <= asciiVal <= 122 else char
+        print("{}".format(upperChar), end="")
+    print()
