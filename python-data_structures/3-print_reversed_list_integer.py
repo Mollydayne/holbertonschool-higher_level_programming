34bce538ad2edc7a1fe635264a830c923ffb6dc5
Mollydayne <cl.w.perez@gmail.com>
2025-01-23 12:32:27 +0100

    Indentation correction for task 3. Print a list of integers... in reverse!

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
-        for num in reversed(my_list):
-            print("{:d}".format(num))
+    for num in reversed(my_list):
+         print("{:d}".format(num))
