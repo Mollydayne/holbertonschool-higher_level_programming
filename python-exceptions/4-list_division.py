028188cce62cbded1203f4379285a9151af7570c
Mollydayne <cl.w.perez@gmail.com>
2025-01-23 11:13:19 +0100

    Trying task 4. Divide a list

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = 0
+    resultList = []
+    for i in range(list_length):
+        try:
+            result = list(map(lambda a, b: a / b, my_list_1, my_list_2))
+        except (ValueError, TypeError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            resultList.append(result)
+    return resultList
