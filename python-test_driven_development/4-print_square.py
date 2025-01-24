adbf8cac5bd20a5f93bbd40c5b1f9d27078cb948
Mollydayne <cl.w.perez@gmail.com>
2025-01-24 13:49:00 +0100

    Test try to 3. Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,3 +7,12 @@ def print_square(size):
 
     """The idea is to print a square using # """
 
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+    print()
