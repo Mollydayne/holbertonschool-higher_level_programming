56cf9893cd5795a4ff3eaf3a97b9cb3b69dc7292
Mollydayne <cl.w.perez@gmail.com>
2025-01-24 14:10:17 +0100

    # is not on one line and is making a square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,5 +13,6 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        for j in range(size):
-            print('#', end="")
+        #for j in range(size):
+            #print('#', end="")
+        print("#" * size)
