4592e8b6e90180edc1d70c8ab96440ce8817d992
Mollydayne <cl.w.perez@gmail.com>
2025-01-24 15:17:27 +0100

    WIP task 4. Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This file is the text_indentation function"""
+
+
+def text_indentation(text):
+
+    """The purpose is to print a text with two new lines after each of defined chars : . , ? and :"""
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = ""
+    for char in text:
+        result += char
+        if char in ".?:":
+            print(result.strip())
+            print()
+    if result:
+        print (result.strip())
