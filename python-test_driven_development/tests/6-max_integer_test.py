960f04b812871600b8e80465d5769f0ba0c849d6
Mollydayne <cl.w.perez@gmail.com>
2025-01-26 14:55:35 +0100

    Completing trst file for task 5. Max integer - Unittest  and renaming main file

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,5 +9,19 @@ class TestMaxInteger(unittest.TestCase):
     """Test cases for the max_integer function"""
 
 def test_max_at_end(self):
-        """Test when the max integer is at the end of the list"""
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+def test_max_at_beginning(self):
+    self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+def test_max_in_middle(self):
+    self.assertEqual(max_integer([1, 4, 2, 3]), 4)
+def test_one_negative_number(self):
+    self.assertEqual(max_integer([-5]), -5)
+def test_only_negative_numbers(self):
+    self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+def test_list_of_one_element(self):
+    self.assertEqual(max_integer([7]), 7)
+def test_list_is_empty(self):
+    self.assertIsNone(max_integer([]))
+
+if __name__ == "__main__":
+    unittest.main()
