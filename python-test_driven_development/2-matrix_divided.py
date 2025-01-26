a1e2fe0efa7c83bb610f2ab96a1105020a3e6227
Mollydayne <cl.w.perez@gmail.com>
2025-01-26 15:30:22 +0100

    Changing name in the doctest + pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,15 +2,24 @@
 
 """function to divide all elements of a matrix."""
 
+
 def matrix_divided(matrix, div):
 
     """Divides all elements of a matrix by a given number."""
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(
+        isinstance(row, list) for row in matrix
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
-    if not all(isinstance(element, (int, float)) for row in matrix for element in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(
+        isinstance(element, (int, float)) for row in matrix for element in row
+    ):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
