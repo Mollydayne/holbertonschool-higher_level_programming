3919c4f46c5f7b88981e6c54339d44eca685c14a
Mollydayne <cl.w.perez@gmail.com>
2025-01-27 14:02:06 +0100

    Correcting indentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,20 +7,20 @@ class Square:
 
     """This will later print a square"""
 
-def __init__(self, size=0):
-    self.__size = size
+    def __init__(self, size=0):
+        self.__size = size
 
-@property
-def size(self):
-    return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-@size.setter
-def size(self, size):
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-def area(self):
-    return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
