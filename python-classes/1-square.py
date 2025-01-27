3fe9af2462b4f02b59542d41216ecb44ca692239
Mollydayne <cl.w.perez@gmail.com>
2025-01-27 10:59:37 +0100

    Trying task 1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This is the class of a square"""
+
+
+class Square:
+
+    """This will later print a square"""
+
+    def __init__(self, size):
+        self.__size = size
+    pass
