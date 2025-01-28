a453668bf7ab0f04866574733a0c027c1bece5a9
Mollydayne <cl.w.perez@gmail.com>
2025-01-28 17:32:03 +0100

    trying again task 6. How many instances

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -8,9 +8,13 @@ class Rectangle:
 
     """This will later print a rectangle"""
 
+    number_of_instances = 0
+
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
