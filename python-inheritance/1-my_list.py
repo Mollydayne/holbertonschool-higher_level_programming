227d24751c682be18e5535c48c84491cd245d977
Mollydayne <cl.w.perez@gmail.com>
2025-02-03 15:01:11 +0100

    trying task 1. My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""contain the class"""
+
+
+class MyList(list):
+
+    """contain print"""
+
+    def print_sorted(self):
+
+        """print the list"""
+
+        print(sorted(self))
