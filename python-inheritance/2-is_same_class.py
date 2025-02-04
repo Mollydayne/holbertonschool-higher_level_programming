00a8ea499294f7e258505caa51ab6652310c45a6
Mollydayne <cl.w.perez@gmail.com>
2025-02-04 10:14:21 +0100

    task 2. Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Class checker"""
+
+def is_same_class(obj, a_class):
+
+    """return true if it's an instance of the class"""
+
+    if isinstance(obj, a_class):
+        return type(obj) is a_class
