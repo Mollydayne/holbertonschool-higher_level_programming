8690cbf552f0f4f7f410d0d10754716cbb8e862e
Mollydayne <cl.w.perez@gmail.com>
2025-02-04 16:05:10 +0100

    changing elif for if for task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -2,17 +2,20 @@
 
 """Following of basegeometry class"""
 
+
 class BaseGeometry:
 
+    """
+    Basegeometry class
+    """
     def area(self):
-
-        """raising an exception"""
-
+        """
+        raising an exception
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError('{} must be an integer'.format(name))
-        elif value <= 0:
+        if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
-        return value
