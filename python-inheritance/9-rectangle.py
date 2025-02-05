7f8f866f0283fcdefb243422701a5f2344171d56
Mollydayne <cl.w.perez@gmail.com>
2025-02-05 10:01:13 +0100

    trying task 9. Full rectangle  and pycodestyle for task 8

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -21,3 +21,15 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        returns the area of a retangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        returns a fstring of rectangle
+        """
+        return f"[rectangle] {self.__width}/{self.__height}"
