c2a0eac2b9240b685204d9d286b93387858f9bb0
Mollydayne <cl.w.perez@gmail.com>
2025-02-05 15:36:59 +0100

    trying task 2. Extending the Python List with Notifications

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+VerboseList
+"""
+
+
+class VerboseList(list):
+
+    """
+    Creating this with the built in list
+    """
+
+    def append(self, item):
+        """
+        print a message when append
+        """
+        super().append(item)
+        print("Added [{}] to the list".format(item))
+
+    def extend(self, item):
+        """
+        print a message when extend
+        """
+        super().extend(item)
+        print("Extended the list with [{}] items".format(item))
+
+    def remove(self, item):
+        """
+        print a message when remove
+        """
+        super().remove(item)
+        print("Removed [{}] from the list".format(item))
+
+    def pop(self, index=-1):
+        """
+        print a message when pop
+        """
+        super().pop(index)
+        print("Popped [{}] from the list".format(self))
