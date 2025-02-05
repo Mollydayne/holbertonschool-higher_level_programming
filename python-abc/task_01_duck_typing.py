9b194767d44492560084cfc6239d5723a59e1fa4
Mollydayne <cl.w.perez@gmail.com>
2025-02-05 15:09:07 +0100

    trying task 1. Shapes, Interfaces, and Duck Typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+"""
+Class for the circle task
+"""
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+
+    """
+    area and perimeter
+    """
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+
+    """
+    circle calculations
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return pi * (self.radius * self.radius)
+
+    def perimeter(self):
+        return abs((2 * self.radius) * pi)
+
+class Rectangle(Shape):
+    """
+    Rectangle!
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return self.__width + self.__height
+
+def shape_info(shape):
+
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
