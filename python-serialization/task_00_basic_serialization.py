61c80a06c9efbe4d095caef4fe7c1e9273396e60
Mollydayne <cl.w.perez@gmail.com>
2025-02-11 10:54:22 +0100

    Adding shebang for T0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Module for task 0 - Basic Serialization
+"""
+
 import json
 
 def serialize_and_save_to_file(data, filename):
-    # Your code here to serialize and save data to the specified file
     data = {"name": "John Doe","age": 30,"city": "New York"}
 
     with open("data.json", "w") as file:
         json.dump(data, file, indent=4)
 
 def load_and_deserialize(filename):
-    # Your code here to load and deserialize data from the specified file
     with open("data.json", "r") as file:
         loaded_data = json.load(file)
 
