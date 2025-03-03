ebdd3be19e0e168edb5600d76f60992507345e98
Mollydayne <cl.w.perez@gmail.com>
2025-03-03 12:24:21 +0100

    Another try for task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,14 +1,14 @@
 -- script that creates a table second_table in the database hbtn_0c_0 - task 9. Full creation
 
-CREATE TABLE IF NOT EXISTS second_table
-(
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
     name VARCHAR(256),
-    score INT PRIMARY KEY
+    score INT,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO second_table(ID, name, score)
-VALUES('1', 'John', '10');
-VALUES('2', 'Alex', '3');
-VALUES('3', 'Bob', '14');
-VALUES('4', 'George', '8');
+INSERT INTO second_table(ID, name, score) VALUES
+('1', 'John', '10');
+('2', 'Alex', '3');
+('3', 'Bob', '14');
+('4', 'George', '8');
