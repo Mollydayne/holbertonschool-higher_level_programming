5b3be4dc95e4a9ada2f16a7e7f5c1235629916c8
Mollydayne <cl.w.perez@gmail.com>
2025-03-05 10:42:29 +0100

    Another try for task 2 : removing the error message in case of no state found

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -13,31 +13,27 @@ if __name__ == "__main__":
             <database_name> <state_name>")
         sys.exit(1)
 
-    try:
-        mysql_user = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name = sys.argv[4]
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
+    try:
         db = MySQLdb.connect(
             host="localhost",
             user=mysql_user,
             password=mysql_password,
             database=database_name,
-            charset="utf8mb4"
         )
 
         cursor = db.cursor()
 
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC;"
-        cursor.execute(query, (state_name,))
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+        cursor.execute(query)
 
         results = cursor.fetchall()
-        if not results:
-            print("No Sates matching ur request!")
-        else:
-            for row in results:
-                print(row)
+        for row in results:
+            print(row)
 
         cursor.close()
         db.close()
