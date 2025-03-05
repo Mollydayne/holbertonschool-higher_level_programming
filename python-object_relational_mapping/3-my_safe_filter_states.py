7fc7c54dc8fc82ef172a0b842c4cb9c33209e0f3
Mollydayne <cl.w.perez@gmail.com>
2025-03-05 10:56:18 +0100

    Trying task 3 sql injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Task 3. SQL Injection...
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: ./3-my_safe_filter_states.py <mysql_username> <mysql_password> \
+            <database_name> <state_name>")
+        sys.exit(1)
+
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            user=mysql_user,
+            password=mysql_password,
+            database=database_name
+        )
+
+        cursor = db.cursor()
+
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as err:
+        print(f"Erreur MySQL : {err}")
+        sys.exit(1)
