abdb9c165605ab51f43f1a65a0b8b6a14f40dc91
Mollydayne <cl.w.perez@gmail.com>
2025-03-06 14:58:52 +0100

    Trying tasks until task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Task 9. Contains `a`
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from sqlalchemy import or_
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+state = session.query(State).filter(State.name.ilike(sys.argv[4])).first()
+
+if state:
+    print("{}: {}".format(state.id, state.name))
+else:
+    print("Not found")
+    session.close()
