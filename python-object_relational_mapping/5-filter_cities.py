db32a11b8560779ba420f70d56d5c109e86016b8
Mollydayne <cl.w.perez@gmail.com>
2025-03-06 15:03:17 +0100

    Pycodestyle for task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -20,11 +20,7 @@ if __name__ == "__main__":
                JOIN states ON cities.state_id = states.id
                WHERE states.name = %s
                ORDER BY cities.id ASC;""", (sys.argv[4], ))
-
-
-
     print(", ".join([row[0] for row in cursor.fetchall()]))
 
-
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
