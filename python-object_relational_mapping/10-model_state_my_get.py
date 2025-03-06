51bbec7a7acb807e90d2d2a2671ab17097fe4404
Mollydayne <cl.w.perez@gmail.com>
2025-03-06 15:06:48 +0100

    indentation on task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-state = session.query(State).filter(State.name.ilike(sys.argv[4])).first()
+    state = session.query(State).filter(State.name.ilike(sys.argv[4])).first()
 
 if state:
     print("{}: {}".format(state.id, state.name))
